add2f7ea4d7f0ce7db698dc2c7d501feae37c953
APavlov <markmarkovkin@gmail.com>
2023-02-13 10:31:27 +0300

    add users entered params. array data and length limit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,27 @@
 // При решении не рекомендуется пользоваться коллекциями 
 // лучше обойтись исключительно массивами
 
+
+//Метод пользовательского ввода нескольких значений через разделитель
+string[] UserEnteredArray(string text, char delim)
+{
+    //ожидаем пользовательского ввода
+    Console.WriteLine(text);
+    string usersText = Console.ReadLine() ?? string.Empty;
+    //преобразуем строку в строковый массив и возвращаем его
+    return usersText.Split(delim, System.StringSplitOptions.RemoveEmptyEntries);
+}
+
+//Метод пользовательского ввода для целых чисел
+int InsertDigit(string text) 
+{
+    int result;bool parse;
+    Console.WriteLine(text);
+    parse = Int32.TryParse(Console.ReadLine(), out result);
+    if (!parse || result <= 0) result = InsertDigit(text);//Если пользователь ввел некорректное значение вызываем повтороно метод.
+    return result;
+}
+
 //Метод преобразования массива в строку
 string ArrayToString(string[] array)
 {
@@ -17,7 +38,7 @@ string ArrayToString(string[] array)
 }
 
 // Возвращаем массив с элементами по количеству знаков меньше или равными limit
-string[] RefillArray(string[] array, int limit = 0, string tmpSeparate = "<<")
+string[] RefillArray(string[] array, int limit, string tmpSeparate = "<<")
 {
     string tmpText = string.Empty;
     for (int i = 0; i < array.Length; i++)
@@ -30,12 +51,25 @@ string[] RefillArray(string[] array, int limit = 0, string tmpSeparate = "<<")
     return resArray;
 }
 
-int n = 3; // Максимальное количество символов в значении массива
-string[] insArray = {"hello","world",":>)","smt","end","start"};
+// int n = 3; // Максимальное количество символов в значении массива
+// string[] insArray = {"hello","world",":>)","smt","end","start"};
+
+// Запрашиваем у пользователя ввести данные через разделитель
+char delimText = ',';
+string messageText = "Введите значние массива через запятую `,`:";
+string[] insArray = UserEnteredArray(messageText, delimText);
+string basicArrText = ArrayToString(insArray);
+
+// Запрашиваем у пользователя максимальную длину элемента массива
+messageText = "\nЗадайте максимальную длинну элемента массива (max,>0):";
+int n = InsertDigit(messageText);
 
 // формируем новый массив с элементами длинна которых меньше или равна заданой длине
 string[] newArray = RefillArray(insArray,n,"<<#>>"); 
 string newArrText = ArrayToString(newArray);
 
-
+// Выводим на экран все данные
+Console.WriteLine( $"\nНачальный массив данных\n{basicArrText}\n" );
+Console.WriteLine( $"Максимальная длинна элемента массива {n}\n" );
 Console.WriteLine( $"Конечный массив данных.\n{newArrText}\n" );
+
